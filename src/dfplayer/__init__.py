b1c1dfdd02db0bd9478e8777bb8aedd50bc94746
Sebastian Romero <s.romero@arduino.cc>
2024-12-08 14:25:58 +0100

    Add import to package

---
+++ src/dfplayer/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.0.0'
+__author__ = "Sebastian Romero"
+__license__ = "MPL 2.0"
+__maintainer__ = "Sebastian Romero"
+
+# Import core classes and/or functions to expose them at the package level
+from .dfplayer import DFPlayer
